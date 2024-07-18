2acf6ca22e3549c0fc99a9c2bc0fe585017a666c
Chaye Ralph <chaye.ralph@nwktc.edu>
2024-07-18 13:49:58 -0600

    streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ def create_globe_icon():
     ax.set_axis_off()
 
     # Draw the globe (circle)
-    circle = plt.Circle((0.5, 0.5), 0.4, color='deepskyblue', ec='black', lw=2)
+    circle = plt.Circle((0.5, 0.5), 0.4, color='deepskyblue', ec='black', lw=1)
     ax.add_patch(circle)
 
     # Draw simple continents
